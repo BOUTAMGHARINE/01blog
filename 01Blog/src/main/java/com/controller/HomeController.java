1dad114c452113d08e0412de267b5c8f29afe6cd
aboutamgh <anouarboutamgharin@gmail.com>
2025-12-11 02:00:10 +0100

    structer

---
+++ 01Blog/src/main/java/com/controller/HomeController.java
@@ -0,0 +1,13 @@
+package com.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+
+    @GetMapping("/")
+    public String home() {
+        return "<h1>my first runing projet spring boot!</h1>";
+    }
+}
